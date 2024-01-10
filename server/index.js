8679c6b18f6e78d39c64c77a63cc758b9bd8105c
Mohitk786 <118011496+Mohitk786@users.noreply.github.com>
2024-01-11 01:18:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   // Allow requests from your specific domain
-  res.header('Access-Control-Allow-Origin', 'https://smart-study-hub.netlify.app');
+  res.header('Access-Control-Allow-Origin', process.env.DOMAIN`);
 
   // Allow credentials (cookies, authorization headers, etc.)
   res.header('Access-Control-Allow-Credentials', true);
